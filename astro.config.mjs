50b1caccaef097cc81ce4f8d99830f9231bf37a7
Gonzalo Ayuso <gonzalo123@gmail.com>
2026-08-27 16:11:11 +0200

    fix: update BASE path for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
-const site = process.env.SITE || 'https://example.github.io';
-const base = process.env.BASE || '';
+const site = process.env.SITE || 'https://gonzalo123.github.io';
+const base = process.env.BASE || '/tech-radarnews/';
 
 export default defineConfig({ site, base, integrations: [sitemap()] });
